3e3501e80a6e95125a4a54ef943452857d87624b
GithubGw-Js <rgw.22.10@gmail.com>
2020-01-02 17:04:35 -1200

    pemberitahuan

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// jquery panggil sebuah element page scroll pada saat di klik jalankan fungsi
+$('.page-scroll').on('click', function () {
+	// Ambil href
+
+});
+
+window.alert ('Website ini masih dalam pengembangan')
