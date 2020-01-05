5e156c6940028500b31fea5cf56c7d8cfadd2b0a
GithubGw-Js <rgw.22.10@gmail.com>
2020-01-04 19:20:31 -1200

    Innitial commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,45 @@
-// jquery panggil sebuah element page scroll pada saat di klik jalankan fungsi
+// parallax
+$(window).on('load', function(){
+	$('.kiri').addClass('kiri-go');
+	$('.kanan').addClass('kanan-go')
+});
 
-//
-window.alert ('Website ini masih dalam pengembangan')
+$(window).scroll(function(){
+	var wScroll = $(this).scrollTop();
+
+	$('.jumbotron img').css({
+		'transform': 'translate(0px, '+ wScroll / 4 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform': 'translate(0px, '+ wScroll / 2 +'%)'
+	});
+
+	$('.jumbotron p').css({
+		'transform': 'translate(0px, '+ wScroll / 1.2 +'%)'
+	});
+
+
+	// portfolio
+	if( wScroll > $('.portfolio').offset().top - 600 ) {
+		
+		$('.portfolio .thumbnail').each(function(i) {
+			setTimeout(function() {
+				$('.portfolio .thumbnail').eq(i).addClass('muncul');
+			}, 180 * (i+1))
+
+		});
+
+	}
+
+});
+
+
+//notification
+//window.alert ("Welcome to my portfolio")
+
+
+
+
+
+
